3381d6cf4847e3de1cc4495774722abbf90fa68f
Shantanu Sutar <shantanusutar17@gmail.com>
2026-08-07 19:17:07 +0530

    feat: complete restaurant entity and ingredient domain model

---
+++ src/main/java/com/shantanu/model/ContactInformation.java
@@ -0,0 +1,12 @@
+package com.shantanu.model;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
